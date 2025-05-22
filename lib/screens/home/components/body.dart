205d4085c66191f74c64e761fdf0c90a026741ad
bimbim1214 <bimoadityapangestu@gmail.com>
2025-05-22 22:09:37 +0700

    membuat halman body

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:plantapp/contants.dart';
+
+
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // It will provie us total height  and width of our screen
+    Size size = MediaQuery.of(context).size;
+    // it enable scrolling on small device
+    return SingleChildScrollView(
+      
+    );
+  }
+}
