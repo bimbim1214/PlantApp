6182106003d9486eb6a77e1011082293e86e7583
bimbim1214 <bimoadityapangestu@gmail.com>
2025-05-22 22:09:51 +0700

    memanggil body

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:plantapp/screens/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-
+      body: Body(),
     );
   }
 
